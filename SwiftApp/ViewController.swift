f00a24c2fb45191128efbb652e684aeea57d6eb1
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-13 18:12:50 -0800

    My first commit here.

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Jefferey Rigler on 11/13/16.
+//  Copyright © 2016 RiglerDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+      
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
